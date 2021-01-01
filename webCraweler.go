6bd2492155e6b2057c425dfc0a6cad38bb613151
nigamelastic <nigamelastic@gmail.com>
2021-01-01 18:29:49 +0100

    added initial files

---
+++ webCraweler.go
@@ -0,0 +1,12 @@
+package main
+
+//import packages
+import(
+	"fmt" //go base package
+	"io/ioutil" //read/write date from input
+	"net/http" //for sending requests
+	"net/url" //for url formatting
+	"regexp" //regular expressions
+	"runtime"	//GO runtime (used to set the number of threads to be used)
+	"strings"	//string manipulation and testing
+)
